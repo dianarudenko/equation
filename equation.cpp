5cffd1ce51e0b54be24e1fa2ed5676b45ccf79d1
rudenko_diana <dianarudenko@yandex.ru>
2020-09-18 16:33:40 +0300

    w/o classes

--- equation.cpp
+++ equation.cpp
@@ -1,18 +1,29 @@
 #include <math.h>
+#include <stdio.h>
 #include <iostream>
 #include <string.h>
+#include <unistd.h>
+#include <assert.h>
 
-#define EPS 0.00001
+#define EPS 0.000001
 #define INF_ROOTS -1
+#define KOEFFS_NUM 3
 
-#define TEST(func, res)                                                           \
-    do {                                                                          \
-        std::cout << "Testing " << #func << "\nExpected result: " << res << "\n"; \
-        if (func == res) {                                                        \
-            std::cout << "OK\n";                                                  \
-        } else {                                                                  \
-            std::cout << "WRONG\nGot: " << func << "\n";                          \
-        }                                                                         \
+#define TEST(func, res)                                 \
+    do {                                                \
+        std::cout << "Expected value:\n" << res << "\n";\
+        if (func == res) {                              \
+            puts ("OK");                                \
+        } else {                                        \
+            std::cout << "WRONG\nGot: " << func << "\n";\
+        }                                               \
+    } while (0)
+
+#define INIT_3(array, a, b, c)  \
+    do {                        \
+        array[0] = a;           \
+        array[1] = b;           \
+        array[2] = c;           \
     } while (0)
 
 enum exceptions {INF_ROOTS_ERR, OUT_OF_ROOTS_RANGE};
@@ -51,215 +62,186 @@ bool isZero (double num) {
     return abs (num) < EPS;
 }
 
+const double * solverInit () {
+    double * koeff = new double [KOEFFS_NUM];
+    printf ("Please, enter the coefficients\n");
+    bool incorrectInput = true;
+    while (incorrectInput) {
+        incorrectInput = false;
+        for (int i = 0; i < KOEFFS_NUM; i++) {
+            int num = 0;
+            if ((num = scanf ("%lg", koeff + i)) == EOF) { //TODO
+                incorrectInput = true;
+                printf ("Incorrect input! Try again\n");
+                break;
+            } else if (num == 0) {
+                i --;
+                char a;
+                scanf("%c", &a);
+            }
+        }
+    }
+    return koeff;
+}
+
 /*!
-* Класс содержит функции, предназначенные для вычисления корней квадратного уравнения.
+* Данная функция предназначена для вычисления дискриминанта уравнения.
+* \param koeff Указатель на массив с коэффициентами уравнения. Размер массива должен быть равен 3.
+* \return Возвращает значение дискриминанта.
 */
 
-class EquationSolver {
+double discriminant (const double koeff [KOEFFS_NUM]) {
+    return koeff[1] * koeff[1] - 4 * koeff[0] * koeff[2];
+}
+
+/*!
+* Данная функция решает квадраное уравнение с указанными коэффициентами.
+* \param koeff Указаель на массив с коэффициентами уравнения. Размер массива должен быть равен 3.
+* \param roots Указатель на массив, в котром возвращаются корни уравнения. Размер массива равен числу корней.\
+* Если корней нет или их бесконечно много, roots = NULL.
+* \param print Указывает, нужно ли выводить результат на стандартный поток вывода. Значение по умолчанию true.
+* \return Количество корней уравнения.
+*/
+
+int solve (const double koeff [KOEFFS_NUM], double * roots, bool print = true) {
     int numOfRoots = 0;
-    double x1 = 0.0;
-    double x2 = 0.0;
-
-public:
-
-    /*!
-    * Данный метод получает на вход коэффициенты уравнения, решает данное уравнение и \
-    * записывает результат в соответствующие поля класса.
-    */
-
-    void solve (const double & a, const double & b, const double & c) {
-        if (isZero(a)) {
-            if(isZero(b)) {
-                if (isZero(c)) {
-                    numOfRoots = INF_ROOTS;
-                } else /* c != 0 */ {
-                    numOfRoots = 0;
+    roots = NULL;
+    if (isZero(koeff[0])) {
+        if(isZero(koeff[1])) {
+            if (isZero(koeff[2])) {
+                numOfRoots = INF_ROOTS;
+                if (print) {
+                    printf ("Infinite roots\n");
                 }
-            } else /* b != 0 */ {
-                numOfRoots = 1;
-                x1 = -c / b;
-            }
-        } else /* a != 0 */ {
-            double d = discriminant (a, b, c);
-            if (d < 0) {
+            } else /* koeff[2] != 0 */ {
                 numOfRoots = 0;
-            } else if (isZero(d)) {
-                numOfRoots = 1;
-                x1 = -b / (2 * a);
-            } else /* d > 0 */ {
-                numOfRoots = 2;
-                x1 = (-b + sqrt(d)) / (2 * a);
-                x2 = (-b - sqrt(d)) / (2 * a);
-            }
-        }
-    }
-
-    /*!
-    * Данный метод предназначен для вычисления дискриминанта уравнения. Параметрами являются коэффициенты уравненияю.
-    * \return Возвращает значение дискриминанта
-    */
-
-    double discriminant (const double & a, const double & b, const double & c) const {
-        return b * b - 4 * a * c;
-    }
-
-    /*!
-    * Данный метод предназначен для вывода корней уравнения, выводит настоящее состояние класса.
-    */
-
-    void print () {
-        switch (numOfRoots) {
-            case INF_ROOTS:
-                std::cout << "Infinite roots\n";
-                break;
-            case 0:
-                std::cout << "No roots\n";
-                break;
-            case 1:
-                std::cout << "One root:\nx: " << x1 << "\n";
-                break;
-            case 2:
-                std::cout << "Two roots:\nx1: " << x1 << "\nx2: " << x2 << "\n";
-                break;
-        }
-    }
-
-    /*!
-    * Данный метод предназначен для вывода i-го корня уравнения (отсчет ведется с 0), выводит настоящее состояние класса.
-    * При бесконечном множестве корней выдае ошибку.
-    * \param number Номер корня, который нужно получить
-    * \return Значение указанного корня
-    */
-
-    double getRoot (int number) const {
-        if (numOfRoots == INF_ROOTS) {
-            throw INF_ROOTS_ERR;
-        }
-        if ((number < 0) || (number > numOfRoots - 1)) {
-            throw OUT_OF_ROOTS_RANGE;
-        }
-        if (number == 0) {
-            return x1;
-        } else {
-            return x2;
-        }
-    }
-
-    /*!
-    * Данный метод предназначен для вывода корней уравнения, выводит настоящее состояние класса.
-    * При бесконечном множестве корней выдае ошибку.
-    * \return Указатель на массив элементов типа double, содержащий значения корней уравнения
-    */
-
-    double * getRoots () {
-        if (numOfRoots == INF_ROOTS) {
-            throw INF_ROOTS_ERR;
-        }
-        if (numOfRoots == 0) {
-            return NULL;
-        }
-        double * res = new double [numOfRoots];
-        if (numOfRoots == 1) {
-            res [0] = x1;
-        } else {
-            res [0] = x1;
-            res [1] = x2;
-        }
-        return res;
-    }
-
-    /*!
-    * Данный метод предназначен для вывода количества корней уравнения.
-    */
-
-    int getRootsNumber () {
-        return numOfRoots;
-    }
-
-    /*!
-    * Данный метод предназначен для естирования функции solve данноко класса.
-    * \param a Коэффициент №1
-    * \param b Коэффициент №2
-    * \param c Коэффициент №3
-    * \param num Ожидаемое количество корней
-    * \param x1 Ожидаемое значение корня №1
-    * \param x2 Ожидаемое значение корня №2
-    */
-
-    void testSolve (const double & a, const double & b, const double & c, int num, double x1 = 0, double x2 = 0) {
-        solve (a, b, c);
-        TEST (numOfRoots, num);
-        if ((numOfRoots != INF_ROOTS) && (numOfRoots == num)) {
-            if (num == 1) {
-                TEST (getRoot (0), x1);
-            } else if (num == 2) {
-                double r1 = this -> x1;
-                double r2 = this -> x2;
-                if (((r1 - r2) * (x1 - x2)) < 0) {
-                    TEST (r1, x2);
-                    TEST (r2, x1);
-                } else {
-                    TEST (r1, x1);
-                    TEST (r2, x2);
+                if (print) {
+                    printf ("Zero roots\n");
                 }
             }
+        } else /* koeff[1] != 0 */ {
+            numOfRoots = 1;
+            roots = new double [numOfRoots];
+            roots[0] = -koeff[2] / koeff[1];
+            if (print) {
+                printf ("One root:\n%lg\n", roots[0]);
+            }
+        }
+    } else /* koeff[0] != 0 */ {
+        double d = discriminant (koeff);
+        if (isZero(d)) {
+            numOfRoots = 1;
+            roots = new double [numOfRoots];
+            roots[0] = -koeff[1] / (2 * koeff[0]);
+            if (print) {
+                printf ("One root:\n%lg\n", roots[0]);
+            }
+        } else if (d < 0) {
+            numOfRoots = 0;
+            if (print) {
+                printf ("Zero roots\n");
+            }
+        } else /* d > 0 */ {
+            numOfRoots = 2;
+            roots = new double [numOfRoots];
+            roots[0] = (-koeff[1] + sqrt(d)) / (2 * koeff[0]);
+            roots[1] = (-koeff[1] - sqrt(d)) / (2 * koeff[0]);
+            if (print) {
+                printf ("Two roots:\n%lg\n%lg\n", roots[0], roots[1]);
+            }
         }
     }
+    return numOfRoots;
+}
 
-    /*!
-    * Данный метод предназначен для естирования функции discriminant данноко класса.
-    * \param a Коэффициент №1
-    * \param b Коэффициент №2
-    * \param c Коэффициент №3
-    * \param result Ожидаемый результат
-    */
+//-------------------------------------------------------------------------------------------------------------
 
-    void testDiscriminant (const double & a, const double & b, const double & c, const double & result) {
-        TEST (discriminant (a, b, c), result);
+/*!
+* Данная функция предназначена для тестирования функции discriminant().
+* \param koeff Указатель на массив с коэффициентами уравнения. Размер массива должен быть равен 3.
+* \param expected Ожидаемый результат
+*/
+
+void testDiscriminant (const double koeff [KOEFFS_NUM], const double expected) {
+    printf ("testDiscriminant: Testing calculated discriminant.\n");
+    TEST (discriminant (koeff), expected);
+}
+
+/*!
+* Данная функция предназначена для тестирования функции solve().
+* \param koeff Указатель на массив с коэффициентами уравнения. Размер массива должен быть равен 3.
+* \param num Ожидаемое количество корней, если корней бесконечно много,\
+* этот параметр должен быть равен -1
+* \param expected Указатель на массив, в котром возвращаются корни уравнения. Размер массива равен числу корней.\
+* Если корней нет или их бесконечно много, expected = NULL.
+*/
+
+void testSolve (const double koeff [KOEFFS_NUM], int num, const double * expected) {
+    double actual [2];
+    assert ((INF_ROOTS <= num) && (num <= 2));
+    printf ("testSolve: Testing if size of the 'expected' param equals to specified number of roots.\n");
+    TEST (sizeof (expected), num);
+    printf ("testSolve: Testing if calculated number of roots equals to specified one.\n");
+    TEST (solve (koeff, actual, false), num);
+    if ((num != 0) && (num != INF_ROOTS) && (sizeof (actual) == num)) {
+        if (num == 1) {
+            printf ("testSolve: Testing x.\n");
+            TEST (actual[0], expected[0]);
+        } else /* num = 2 */ {
+            if (((actual[0] - actual[1]) * (expected[0] - expected[1])) < 0) {
+                printf ("testSolve: Testing x1.\n");
+                TEST (actual[0], expected[1]);
+                printf ("testSolve: Testing x2.\n");
+                TEST (actual[1], expected[0]);
+            } else {
+                printf ("testSolve: Testing x1.\n");
+                TEST (actual[0], expected[0]);
+                printf ("testSolve: Testing x2.\n");
+                TEST (actual[1], expected[1]);
+            }
+        }
     }
-};
+}
+
+/*!
+* Данная функция предназначена для общего тестирования программы.
+*/
+
+void test () { //TODO file
+    double koeff [KOEFFS_NUM];
+    double roots [2];
+    double root;
+    INIT_3 (koeff, 0, 0, 0);
+    testSolve (koeff, INF_ROOTS, NULL);
+    INIT_3 (koeff, 0, 0, 1);
+    testSolve (koeff, 0, NULL);
+    INIT_3 (koeff, 1, 2, 1);
+    root = -1;
+    testDiscriminant (koeff, 0);
+    testSolve (koeff, 1, &root);
+    INIT_3 (koeff, 1, -2, 1);
+    root = 1;
+    testDiscriminant (koeff, 0);
+    testSolve (koeff, 1, &root);
+    INIT_3 (koeff, 1, 4, 3);
+    roots[0] = -1;
+    roots[1] = -3;
+    testDiscriminant (koeff, 1);
+    testSolve (koeff, 2, roots);
+}
 
 /*!
 * Без указания режима просто считает решение уравнения
 * Режимы:
-* -test -- Запускает готовые тесты
-* -test-now -- Тест в реальном времени
-* -test-discrimiant -- Тест дискриминанта
+* -test -- Запускает тест программы
 */
 
 int main (int argc, char ** argv) {
-    EquationSolver solver;
     if (argc > 1) {
-        if (strcmp (argv [1], "-test") == 0) {
-            solver.testSolve (0, 0, 0, INF_ROOTS);
-            solver.testSolve (1, 2, 1, 1, -1);
-            solver.testSolve (1, -2, 1, 1, 1);
-            solver.testSolve (1, 4, 3, 2, -1, -3);
-        } else if (strcmp (argv [1], "-test-now") == 0) {
-            std::cout << "Please, enter the coefficients\n";
-            double a = 0.0, b = 0.0, c = 0.0, x1 = 0.0, x2 = 0.0;
-            int num = 0;
-            std::cin >> a >> b >> c;
-            std::cout << "Please, enter number of the roots\n";
-            std::cin >> num;
-            if ((num != INF_ROOTS) && (num != 0)) {
-                std::cout << "Please, enter the roots\n";
-                std::cin >> x1 >> x2;
-            }
-            solver.testSolve (a, b, c, num, x1, x2);
-        } else if (strcmp (argv [1], "-test-disc") == 0) {
-            std::cout << "Please, enter the coefficients\n";
-            double a = 0.0, b = 0.0, c = 0.0, res = 0.0;
-            std::cin >> a >> b >> c;
-            std::cout << "Please, enter the result\n";
-            std::cin >> res;
-            solver.testDiscriminant (a, b, c, res);
+        if (strcmp (argv[1], "-test")) {
+            test ();
         }
     } else {
-        double a = 0.0, b = 0.0, c = 0.0;
-        std::cout << "Please, enter the coefficients\n";
-        std::cin >> a >> b >> c;
-        solver.solve (a, b, c);
-        solver.print ();
+        solve (solverInit(), NULL);
     }
 }
